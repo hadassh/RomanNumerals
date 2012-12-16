6ec01da0de86640f73d64c52f1dc7d91741a1a10
hadassh <hadas_shalom3@walla.com>
2012-12-16 11:38:14 +0200

    red- test 51 faild

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -13,6 +13,7 @@ public class RomanNumeralUnitTests {
 		assertEquals("II", number.toRoman(2));
 		assertEquals("IV", number.toRoman(4));
 		assertEquals("X", number.toRoman(10));
+		assertEquals("LI", number.toRoman(51));
 		
 	}
 	
